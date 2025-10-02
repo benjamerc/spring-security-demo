662a76475bf78d5d5cdc934e5c3e829664db0abe
Benjamín Merchán <benjamerchan7@gmail.com>
2025-10-02 16:37:37 -0300

    refactor(user): add @UniqueValue to username in UserPartialUpdateRequest

--- src/main/java/com/benjamerc/spring_security_course/users/dto/request/UserPartialUpdateRequest.java
+++ src/main/java/com/benjamerc/spring_security_course/users/dto/request/UserPartialUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.benjamerc.spring_security_course.users.dto.request;
 
+import com.benjamerc.spring_security_course.shared.validation.UniqueValue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
@@ -7,6 +8,7 @@ public record UserPartialUpdateRequest(
 
         @Email(message = "Username must be a valid email address")
         @Size(max = 100, message = "Username cannot exceed 100 characters")
+        @UniqueValue
         String username,
 
         @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
