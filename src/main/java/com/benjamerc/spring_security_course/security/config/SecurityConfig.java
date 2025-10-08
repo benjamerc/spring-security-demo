e32484b64bb413f5cd9aa1fedc43727fd2605489
Benjamín Merchán <benjamerchan7@gmail.com>
2025-10-08 20:58:33 -0300

    refactor(security): remove unnecessary imports

--- src/main/java/com/benjamerc/spring_security_course/security/config/SecurityConfig.java
+++ src/main/java/com/benjamerc/spring_security_course/security/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.benjamerc.spring_security_course.security.core.JwtAuthenticationFilte
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -16,8 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.HttpStatusAccessDeniedHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
