99ca40547ba4447840c513d97882babda755e88e
Benjamín Merchán <benjamerchan7@gmail.com>
2025-10-08 21:16:58 -0300

    refactor(test): remove unnecessary import in UserControllerIT

--- src/test/java/com/benjamerc/spring_security_course/users/controller/UserControllerIT.java
+++ src/test/java/com/benjamerc/spring_security_course/users/controller/UserControllerIT.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
 
