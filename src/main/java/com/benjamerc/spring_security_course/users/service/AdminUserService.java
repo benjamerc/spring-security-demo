d4132bc6fb4dd7dd78e109485f8726033fe60748
Benjamín Merchán <benjamerchan7@gmail.com>
2025-10-08 21:04:03 -0300

    refactor(user): replace return type with CustomPage in AdminUserService.getAllUsers()

--- src/main/java/com/benjamerc/spring_security_course/users/service/AdminUserService.java
+++ src/main/java/com/benjamerc/spring_security_course/users/service/AdminUserService.java
@@ -1,14 +1,14 @@
 package com.benjamerc.spring_security_course.users.service;
 
+import com.benjamerc.spring_security_course.shared.dto.pagination.CustomPage;
 import com.benjamerc.spring_security_course.users.dto.request.AdminUserUpdateRequest;
 import com.benjamerc.spring_security_course.users.dto.response.AdminUserResponse;
 import com.benjamerc.spring_security_course.users.dto.response.AdminUserSummaryResponse;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AdminUserService {
 
-    Page<AdminUserSummaryResponse> getAllUsers(Pageable pageable);
+    CustomPage<AdminUserSummaryResponse> getAllUsers(Pageable pageable);
 
     AdminUserResponse getUserById(Long id);
 
